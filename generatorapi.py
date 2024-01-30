f1f485055381de8a75fc7971f4c23063f7b2bcd6
Sumukin <mfodegov@gmail.com>
2024-01-30 22:00:51 +0700

    generator api v1.1

--- generatorapi.py
+++ generatorapi.py
@@ -1,22 +1,27 @@
 from random import choice
 
 def generate(lenght, numbers=False, letters=False, spsimbols=False, count=1):
+    #проверка на то есть ли у нас символы из которых можно составить пароль
     if (numbers == False) and (letters == False) and (spsimbols == False):
         return('no arguments')
+    #проверка того правильно ли введены данные
     if (count < 1) or (lenght < 1) or ('int' not in str(type(count))) or ('int' not in str(type(lenght))):
         return('wrong argument')
     res = ''
     symbols = ''
-    if numbers == True:
+    #создаем строку из которой потом будем брать символы для пароля
+    if numbers:
         symbols += '1234567890'
-    if letters == True:
+    if letters:
         symbols += 'qwertyuiopasdfghjklzxcvbnm'
-    if spsimbols == True:
+    if spsimbols:
         symbols += '!@#$%^&*()_+=-?:;№".,/|<>'
+    #если нам необходимо сделать одну генерацию
     if count == 1:
         for i in range(lenght):
             res += choice(symbols)
         return(res)
+    #если необходимо сделать больше, чем одну генерацию
     elif count > 1:
         res_m = []
         for j in range(count):
@@ -24,4 +29,4 @@ def generate(lenght, numbers=False, letters=False, spsimbols=False, count=1):
             for i in range(lenght):
                 res += choice(symbols)
             res_m.append(res)
-        return res_m
+        return(res_m)
