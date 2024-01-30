870210c3b8f29e85d6a3ab958f07299107f9874b
JunkCoded <artemshow_yt@mail.ru>
2024-01-30 23:48:20 +0700

    Добавлены новые кнопки

--- ui.py
+++ ui.py
@@ -1,12 +1,26 @@
 import dearpygui.dearpygui as dpg
+import pyperclip
+#import dearpygui.demo as demo
 
 w, h = 600, 300
 
 dpg.create_context()
 dpg.create_viewport(title='DisisentGen', width=w, height=h)
 
+def show_password():
+    print('show')
+
+def hide_password():
+    print('hide')
+
+def copy_password():
+    pyperclip.copy(dpg.get_value('password'))
+
 with dpg.window(label='DisisentGen', tag='main_window'):
-    dpg.add_input_text(label="Password", password=True)
+    dpg.add_input_text(tag='password', label="Password", password=True)
+    dpg.add_button(tag='show', label='Show', callback=show_password)
+    dpg.add_button(tag='hide', label='Hide', show=False, callback=hide_password)
+    dpg.add_button(tag='copy', label='Copy', callback=copy_password)
 
 dpg.set_primary_window('main_window', True)
 
