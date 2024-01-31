46e84c3a5fb5cdb39144b9449447716521d268d5
Sumukin <mfodegov@gmail.com>
2024-01-31 14:20:13 +0700

    generator api 1.3

--- generatorapi.py
+++ generatorapi.py
@@ -1,32 +1,32 @@
 from random import choice
 
-def generate(lenght, numbers=False, letters=False, spsimbols=False, count=1):
+def _gen(length, symbols):
+        res = ''
+        for i in range(length):
+            res += choice(symbols)
+        return res
+
+def generate(length, numbers=False, letters=False, schars=False, count=1):
     #проверка на то есть ли у нас символы из которых можно составить пароль
-    if (numbers == False) and (letters == False) and (spsimbols == False):
+    if not (numbers or letters or schars):
         return('no arguments')
     #проверка того правильно ли введены данные
-    if (count < 1) or (lenght < 1) or (not isinstance(count, int)) or (not isinstance(lenght, int)):
+    if (count < 1) or (length < 1) or (not isinstance(count, int)) or (not isinstance(length, int)):
         return('wrong argument')
-    res = ''
-    symbols = ''
+    charset = ''
     #создаем строку из которой потом будем брать символы для пароля
     if numbers:
-        symbols += '1234567890'
+        charset += '1234567890'
     if letters:
-        symbols += 'qwertyuiopasdfghjklzxcvbnm'
-    if spsimbols:
-        symbols += '!@#$%^&*()_+=-?:;№".,/|<>'
+        charset += 'qwertyuiopasdfghjklzxcvbnm'
+    if schars:
+        charset += '!@#$%^&*()_+=-?:;№".,/|<>'
     #если нам необходимо сделать одну генерацию
     if count == 1:
-        for i in range(lenght):
-            res += choice(symbols)
-        return(res)
+        return _gen(length, charset)
     #если необходимо сделать больше, чем одну генерацию
     elif count > 1:
         res_m = []
         for j in range(count):
-            res = ''
-            for i in range(lenght):
-                res += choice(symbols)
-            res_m.append(res)
-        return(res_m)
+            res_m.append(_gen(length, charset))
+        return res_m
